4e9b7cb9cfe4fe9fc027e1e13c78e03d15ddc546
StasPolV <poliakov.stas.cpp@gmail.com>
2026-08-15 14:03:11 +0300

    feat(CurrencyModel): implement CurrencyModel

---
+++ src/model/CurrencyModel.cpp
@@ -0,0 +1,66 @@
+#include "CurrencyModel.h"
+
+CurrencyModel::CurrencyModel(QObject* parent) : QAbstractListModel(parent) {}
+
+int CurrencyModel::rowCount(const QModelIndex& parent) const
+{
+	if (parent.isValid())
+	{
+		return 0;
+	}
+	return m_currencies.size();
+}
+
+QVariant CurrencyModel::data(const QModelIndex& index, int role) const
+{
+	if (!index.isValid() || index.row() < 0 || index.row() >= m_currencies.size())
+	{
+		return {};
+	}
+
+	const Currency& c = m_currencies.at(index.row());
+
+	switch (role)
+	{
+	case Qt::DisplayRole:
+	case NameRole: return c.name;
+	case CodeRole: return c.code;
+	default: return {};
+	}
+}
+
+Qt::ItemFlags CurrencyModel::flags(const QModelIndex& index) const
+{
+	if (!index.isValid())
+	{
+		return Qt::NoItemFlags;
+	}
+	return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+}
+
+void CurrencyModel::AddCurrency(const Currency& currency)
+{
+	const int row = m_currencies.size();
+	beginInsertRows(QModelIndex(), row, row);
+	m_currencies.append(currency);
+	endInsertRows();
+}
+
+void CurrencyModel::RemoveCurrency(int row)
+{
+	if (row < 0 || row >= m_currencies.size())
+	{
+		return;
+	}
+
+	beginRemoveRows(QModelIndex(), row, row);
+	m_currencies.removeAt(row);
+	endRemoveRows();
+}
+
+void CurrencyModel::SetCurrencies(QVector<Currency> currencies)
+{
+	beginResetModel();
+	m_currencies = currencies;
+	endResetModel();
+}
---
+++ src/model/CurrencyModel.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <QAbstractListModel>
+#include <QString>
+#include <QVector>
+
+struct Currency
+{
+	QString name;
+	QString code;
+};
+
+class CurrencyModel : public QAbstractListModel
+{
+	Q_OBJECT
+public:
+	enum Roles
+	{
+		NameRole = Qt::UserRole + 1,
+		CodeRole
+	};
+
+	explicit CurrencyModel(QObject* parent = nullptr);
+
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+	Qt::ItemFlags flags(const QModelIndex& index) const override;
+
+	void AddCurrency(const Currency& currency);
+	void RemoveCurrency(int row);
+	void SetCurrencies(QVector<Currency> currencies);
+
+private:
+	QVector<Currency> m_currencies;
+};
